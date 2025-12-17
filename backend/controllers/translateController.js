0cc0ddb733244e9247b049c419e91df13cf6dfdf
akstokely <annabeth.stokely@gmail.com>
2025-12-16 22:10:13 -0500

    sending query params

--- backend/controllers/translateController.js
+++ backend/controllers/translateController.js
@@ -1,10 +1,14 @@
+const deepl = require('deepl-node');
 const DEEPL_API_KEY = process.env.DEEPL_API_KEY;
+const deeplClient = new deepl.DeepLClient(DEEPL_API_KEY);
 
 /**
  * Use the DeepL API to translate the given text
  */
 function translate(req, res) {
+    console.log(req.query.test);
     console.log("In translateController");
+
     return res.json({ message: "in translateController" });
 }
 
